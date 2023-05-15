4bfdd4a216cb8131e0d09464ed15d21e87933a56
Radhakrishnan Jayaraman <radhakrishnan3128@gmail.com>
2023-05-15 22:52:50 +0530

    About page edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>HomePage</h1>
-      <div className="ScrollBar">
+      {/* <div className="ScrollBar">
         <p className="auto-scroll">R A D H A K R I S H N A N </p>
-      </div>
+      </div> */}
     </div>
   );
 }
