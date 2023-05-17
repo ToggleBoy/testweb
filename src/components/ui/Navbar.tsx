a5110eea0ef13e47cb14c30a58692b56fb63c2a3
Radhakrishnan Jayaraman <radhakrishnan3128@gmail.com>
2023-05-17 23:22:37 +0530

    navbar responsive

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,22 +1,105 @@
+// import Link from "next/link";
+// import Image from "next/image";
+
+// const Navbar = () => {
+//     return (
+//         <div className="navbar">
+//             <div className="navigation-title">
+//               <Link href={"/"}>
+//                 <Image src="/logo.png" alt="logo" width={100} height={75} priority={true}/></Link>
+//             </div>
+//             <div className="navigation-link">
+//                 <Link href="/"><p>Home</p></Link>
+//                 <Link href="/about"><p>About</p></Link>
+//                 <Link href="/career"><p>Career</p></Link>
+//                 <Link href="/projects"><p>Projects</p></Link>
+//                 <Link href="/contact-me"><p>Contact Me</p></Link>
+//             </div>
+//         </div>
+//      );
+// }
+
+// export default Navbar;
+
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="navigation-title">
-              <Link href={"/"}>
-                <Image src="/logo.png" alt="logo" width={100} height={75} priority={true}/></Link>
-            </div>
-            <div className="navigation-link">
-                <Link href="/"><p>Home</p></Link>
-                <Link href="/about"><p>About</p></Link>
-                <Link href="/career"><p>Career</p></Link>
-                <Link href="/projects"><p>Projects</p></Link>
-                <Link href="/contact-me"><p>Contact Me</p></Link>
-            </div>
-        </div>
-     );
-}
+const Home = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-export default Navbar;
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="naigation-title">
+        <Link href={"/"}>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={100}
+            height={75}
+            priority={true}
+          />
+        </Link>
+      </div>
+      <div className="navigation-link">
+        <Link href="/">
+          <p>Home</p>
+        </Link>
+        <Link href="/about">
+          <p>About</p>
+        </Link>
+        <Link href="/career">
+          <p>Career</p>
+        </Link>
+        <Link href="/projects">
+          <p>Projects</p>
+        </Link>
+        <Link href="/contact-me">
+          <p>Contact Me</p>
+        </Link>
+      </div>
+      <input
+        className="hamburger-navbar"
+        onClick={toggleMobileMenu}
+        type="button"
+        value="| | |"
+      />
+      {isMobileMenuOpen && (
+        <div className="overlay" onClick={closeMobileMenu}>
+          <div className="mobile-menu">
+            <button className="close-button" onClick={closeMobileMenu}>
+              X
+            </button>
+            <Link href="/">
+              <p>Home</p>
+            </Link>
+            <Link href="/about">
+              <p>About</p>
+            </Link>
+            <Link href="/career">
+              <p>Career</p>
+            </Link>
+            <Link href="/projects">
+              <p>Projects</p>
+            </Link>
+            <Link href="/contact-me">
+              <p>Contact Me</p>
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
